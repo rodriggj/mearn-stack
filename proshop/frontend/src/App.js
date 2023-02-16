df0a025544c67413d2f3d231a897f37714f681d9
rodriggj <gabriel.rodriguez.ctr@gmail.com>
2023-02-16 05:53:41 -0700

    adding react router dom

--- proshop/frontend/src/App.js
+++ proshop/frontend/src/App.js
@@ -1,21 +1,26 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import HomeScreen from "./screens/HomeScreen";
+import Homescreen from "./screens/Homescreen";
+import Productscreen from './screens/Productscreen'
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header/>
       <main className="py-3"> 
         <Container>
           <h1>Welcome to Proshop.</h1>
-          <HomeScreen/>
+          <Routes>
+            <Route path="/" element={<Homescreen/>}/>
+            <Route path="/product/:id" element={<Productscreen/>}/>
+          </Routes>
         </Container>
       </main>
       <Footer/>
-    </>
+    </Router>
   );
 }
 
